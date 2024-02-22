788dbf8b2a07b29479541d56db616659e7ce197e
adedamola26 <adesoye.a@northeastern.edu>
2024-02-22 12:19:38 -0500

    Upload dbt models

---
+++ include/dbt/models/transform/dim_datetime.sql
@@ -0,0 +1,17 @@
+WITH datetime_cte AS (  
+  SELECT DISTINCT
+    InvoiceDate AS datetime_id,
+    PARSE_DATETIME('%m/%d/%Y %I:%M %p', InvoiceDate) AS date_part,
+  FROM {{ source('retail', 'raw_invoices') }}
+  WHERE InvoiceDate IS NOT NULL
+)
+SELECT
+  datetime_id,
+  date_part as datetime,
+  EXTRACT(YEAR FROM date_part) AS year,
+  EXTRACT(MONTH FROM date_part) AS month,
+  EXTRACT(DAY FROM date_part) AS day,
+  EXTRACT(HOUR FROM date_part) AS hour,
+  EXTRACT(MINUTE FROM date_part) AS minute,
+  EXTRACT(DAYOFWEEK FROM date_part) AS weekday
+FROM datetime_cte
