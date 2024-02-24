d79a2580bee738758670106de52bc36d821637f2
adedamola26 <adesoye.a@northeastern.edu>
2024-02-24 14:53:57 -0500

    Refactor report_year_invoices.sql to use new table and column names

--- include/dbt/models/report/report_year_invoices.sql
+++ include/dbt/models/report/report_year_invoices.sql
@@ -1,9 +1,9 @@
 SELECT
   dt.year,
   dt.month,
-  COUNT(DISTINCT fi.invoice_id) AS num_invoices,
-  SUM(fi.total) AS total_revenue
-FROM {{ ref('fct_invoices') }} fi
-JOIN {{ ref('dim_datetime') }} dt ON fi.datetime_id = dt.datetime_id
+  COUNT(DISTINCT fi.invoice_key) AS num_invoices,
+  SUM(fi.total_price) AS total_revenue
+FROM {{ ref('fct_invoice_line_value') }} fi
+JOIN {{ ref('dim_datetime') }} dt ON fi.date_key = dt.date_key
 GROUP BY dt.year, dt.month
 ORDER BY dt.year, dt.month
