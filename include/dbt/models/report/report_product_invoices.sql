9a9964eea0fb2d322d144959db5263801151ba5b
adedamola26 <adesoye.a@northeastern.edu>
2024-02-24 14:52:21 -0500

    Refactor report_product_invoices.sql to use new table and column names

--- include/dbt/models/report/report_product_invoices.sql
+++ include/dbt/models/report/report_product_invoices.sql
@@ -1,10 +1,10 @@
 SELECT
-  p.product_id,
+  p.product_key,
   p.stock_code,
   p.description,
   SUM(fi.quantity) AS total_quantity_sold
-FROM {{ ref('fct_invoices') }} fi
-JOIN {{ ref('dim_product') }} p ON fi.product_id = p.product_id
-GROUP BY p.product_id, p.stock_code, p.description
+FROM {{ ref('fct_invoice_line_value') }} fi
+JOIN {{ ref('dim_product') }} p ON fi.product_key = p.product_key
+GROUP BY p.product_key, p.stock_code, p.description
 ORDER BY total_quantity_sold DESC
 LIMIT 10
