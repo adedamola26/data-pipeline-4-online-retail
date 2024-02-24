8e15a7c2fdf5ddddea3b2310c5c586c9bdfda004
adedamola26 <adesoye.a@northeastern.edu>
2024-02-24 14:50:12 -0500

    Refactor report_customer_invoices.sql to use new table and column names

--- include/dbt/models/report/report_customer_invoices.sql
+++ include/dbt/models/report/report_customer_invoices.sql
@@ -1,10 +1,11 @@
 SELECT
-  c.country,
-  c.iso,
-  COUNT(fi.invoice_id) AS total_invoices,
-  SUM(fi.total) AS total_revenue
-FROM {{ ref('fct_invoices') }} fi
-JOIN {{ ref('dim_customer') }} c ON fi.customer_id = c.customer_id
-GROUP BY c.country, c.iso
+  dc.country,
+  dc.iso,
+  COUNT(fi.invoice_key) AS total_invoices,
+  SUM(fi.total_price) AS total_revenue
+FROM {{ ref('fct_invoice_line_value') }} fi
+JOIN {{ ref('dim_invoice') }} di ON fi.invoice_key = di.invoice_key
+JOIN {{ ref('dim_customer') }} dc ON di.customer_key = dc.customer_key
+GROUP BY dc.country, dc.iso
 ORDER BY total_revenue DESC
 LIMIT 10
